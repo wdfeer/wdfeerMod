3f7389280021183ec9ff2a69f62bc55402aa5ec2
wdf33r <49950781+wdf33r@users.noreply.github.com>
2021-07-23 09:48:19 +0300

    Added Kuva resource

---
+++ Items/Kuva.cs
@@ -0,0 +1,41 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using System;
+using Microsoft.Xna.Framework;
+
+namespace wdfeerMod.Items
+{
+    public class Kuva : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Material for some Grineer weapons");
+        }
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            item.rare = 5; // the color that the item's name will be in-game
+            item.maxStack = 99;
+            item.width=16;
+            item.height=15;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.SoulofNight,1);
+            recipe.AddIngredient(ItemID.CursedFlames,2);
+            recipe.AddTile(TileID.AlchemyTable); 
+            recipe.SetResult(this);
+            recipe.AddRecipe(); 
+
+            recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.SoulofNight,1);
+            recipe.AddIngredient(ItemID.Ichor,2);
+            recipe.AddTile(TileID.AlchemyTable); 
+            recipe.SetResult(this);
+            recipe.AddRecipe(); 
+        }
+    }
+}
