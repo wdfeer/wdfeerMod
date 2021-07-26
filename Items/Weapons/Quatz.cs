c2000fbbc6a3e6bcc7efb4e2a70d998e9cfc616b
wdf33r <49950781+wdf33r@users.noreply.github.com>
2021-07-26 17:12:38 +0300

    Added the Quatz
    + Changed Furis recipe and spread

--- Items/Weapons/Quatz.cs
+++ Items/Weapons/Quatz.cs
@@ -6,23 +6,29 @@ using Microsoft.Xna.Framework;
 
 namespace wdfeerMod.Items.Weapons
 {
-    public class Furis : ModItem
+    public class Quatz : ModItem
     {
         Random rand = new Random();
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Right Click to use the Burst fire mode\nIn auto has a 27% Electricity proc chance\nGains a bonus to critical stats in Burst mode");
+        }
         public override void SetDefaults()
         {
-            item.damage = 4; // Sets the item's damage. Note that projectiles shot by this weapon will use its and the used ammunition's damage added together.
+            item.damage = 2; // Sets the item's damage. Note that projectiles shot by this weapon will use its and the used ammunition's damage added together.
+            item.crit = 9;
             item.ranged = true; // sets the damage type to ranged
-            item.width = 16; // hitbox width of the item
-            item.height = 14; // hitbox height of the item
-            item.useTime = 6; // The item's use time in ticks (60 ticks == 1 second.)
-            item.useAnimation = 6; // The length of the item's use animation in ticks (60 ticks == 1 second.)
+            item.width = 32; // hitbox width of the item
+            item.height = 19; // hitbox height of the item
+            item.scale = 0.8f;
+            item.useTime = 4; // The item's use time in ticks (60 ticks == 1 second.)
+            item.useAnimation = 4; // The length of the item's use animation in ticks (60 ticks == 1 second.)
             item.useStyle = ItemUseStyleID.HoldingOut; // how you use the item (swinging, holding out, etc)
             item.noMelee = true; //so the item's animation doesn't do damage
             item.knockBack = 0; // Sets the item's knockback. Note that projectiles shot by this weapon will use its and the used ammunition's knockback added together.
             item.value = 1500; // how much the item sells for (measured in copper)
             item.rare = 3; // the color that the item's name will be in-game
-            item.UseSound = SoundID.Item11; // The sound that this item plays when used.
+            item.UseSound = SoundID.Item11.WithVolume(0.6f); // The sound that this item plays when used.
             item.autoReuse = true; // if you can hold click to automatically use it again
             item.shoot = 10; //idk why but all the guns in the vanilla source have this
             item.shootSpeed = 16f; // the speed of the projectile (measured in pixels per frame)
@@ -33,9 +39,8 @@ namespace wdfeerMod.Items.Weapons
         {
             ModRecipe recipe = new ModRecipe(mod);
             // ItemType<ExampleItem>() is how to get the ExampleItem item, 10 is the amount of that item you need to craft the recipe
-            recipe.AddIngredient(ItemID.ShadowScale, 16);
-            recipe.AddIngredient(331, 2);
-            recipe.AddRecipeGroup("Wood", 8);
+            recipe.AddIngredient(ItemID.ShadowScale, 12);
+            recipe.AddIngredient(ItemID.TungstenBar, 8);
             // You can use recipe.AddIngredient(ItemID.TheItemYouWantToUse, the amount of items needed); for a vanilla item.
             recipe.AddTile(TileID.Hellforge); // Set the crafting tile to ExampleWorkbench
             recipe.SetResult(this); // Set the result to this item (ExampleSword)
@@ -43,20 +48,52 @@ namespace wdfeerMod.Items.Weapons
 
             recipe = new ModRecipe(mod);
             // ItemType<ExampleItem>() is how to get the ExampleItem item, 10 is the amount of that item you need to craft the recipe
-            recipe.AddIngredient(ItemID.TissueSample, 16);
-            recipe.AddIngredient(331, 2);
-            recipe.AddRecipeGroup("Wood", 8);
+            recipe.AddIngredient(ItemID.ShadowScale, 12);
+            recipe.AddIngredient(ItemID.SilverBar, 8);
             // You can use recipe.AddIngredient(ItemID.TheItemYouWantToUse, the amount of items needed); for a vanilla item.
             recipe.AddTile(TileID.Hellforge); // Set the crafting tile to ExampleWorkbench
             recipe.SetResult(this); // Set the result to this item (ExampleSword)
             recipe.AddRecipe(); // When your done, add the recipe
-
         }
-
+        public override bool AltFunctionUse(Player player)
+        {
+            return true;
+        }
+        public override bool CanUseItem(Player player)
+        {
+            if (player.altFunctionUse == 2)
+            {
+                item.useTime = 23;
+                item.useAnimation = 23;
+                item.crit = 23;
+                item.shootSpeed = 20f;
+                item.autoReuse = false;
+                item.UseSound = SoundID.Item11;
+            }
+            else
+            {
+                item.useTime = 4;
+                item.useAnimation = 4;
+                item.crit = 9;
+                item.shootSpeed = 16f;
+                item.autoReuse = true;
+                item.UseSound = SoundID.Item11.WithVolume(0.6f);
+            }
+            return base.CanUseItem(player);
+        }
         public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
         {
             Vector2 spread = new Vector2(speedY, -speedX);
-            Projectile.NewProjectile(position, new Vector2(speedX, speedY) + spread * Main.rand.NextFloat(-0.025f, 0.025f), type, damage, knockBack, Main.LocalPlayer.cHead);
+
+            for (int i = 0; i < (player.altFunctionUse == 2 ? 4 : 1); i++)
+            {
+                var proj = Main.projectile[Projectile.NewProjectile(position, new Vector2(speedX, speedY) + spread * Main.rand.NextFloat(-0.012f, 0.012f), type, damage, knockBack, Main.LocalPlayer.cHead)];
+                var globalProj = proj.GetGlobalProjectile<Projectiles.wdfeerGlobalProj>();
+                if (player.altFunctionUse == 2)
+                    globalProj.critMult = 1.25f;
+                else globalProj.electroChance = 27;
+            }
+
             return false;
         }
     }
