3592a9a47e8a0d2f65a902b247ae8f3bc8c42490
wdf33r <49950781+wdfeer@users.noreply.github.com>
2021-09-01 13:48:07 +0300

    Added 5 static methods to the wdfeerMod class to simplify spawning dusts

--- wdfeerMod.cs
+++ wdfeerMod.cs
@@ -1,9 +1,54 @@
+using System;
 using Terraria.ModLoader;
 using Terraria;
+using Microsoft.Xna.Framework;
 namespace wdfeerMod
 {
     public class wdfeerMod : Mod
     {
-
+        public static void NewDustsCircleEdge(int count, Vector2 center, float radius, int type, Action<Dust> edit = null)
+        {
+            NewDustsCustom(count,
+             () => Dust.NewDustPerfect(center + Main.rand.NextVector2CircularEdge(radius, radius), type),
+             edit);
+        }
+        /// <summary>
+        /// Spawns dusts in a circle and applies a velocity from the center of the circle to them
+        /// </summary>
+        public static void NewDustsCircleFromCenter(int count, Vector2 center, float radius, int type, float velocityMult, Action<Dust> edit = null)
+        {
+            NewDustsCircle(count, center, radius, type, (dust) =>
+            {
+                dust.velocity += Vector2.Normalize(dust.position - center) * velocityMult;
+                if (edit != null)
+                    edit(dust);
+            });
+        }
+        public static void NewDustsCircle(int count, Vector2 center, float radius, int type, Action<Dust> edit = null)
+        {
+            NewDustsCustom(count,
+             () => Dust.NewDustPerfect(center + Main.rand.NextVector2Circular(radius, radius), type),
+             edit);
+        }
+        public static void NewDustsPerfect(int count, Vector2 position, int type, Action<Dust> edit = null)
+        {
+            NewDustsCustom(count,
+             () => Dust.NewDustPerfect(position, type),
+             edit);
+        }
+        /// <summary>
+        /// Spawns dusts with the create function, calls the edit on each of them (if edit is specified)
+        /// </summary>
+        /// <param name="count">Number of dusts to spawn</param>
+        /// <param name="create">Function that returns a dust</param>
+        public static void NewDustsCustom(int count, Func<Dust> create, Action<Dust> edit = null)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Dust dust = create();
+                if (edit != null)
+                    edit(dust);
+            }
+        }
     }
 }
