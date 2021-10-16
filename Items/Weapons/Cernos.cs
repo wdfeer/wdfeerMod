73b2ed882a0b64faccba87650f67696185b954a6
wdf33r <49950781+wdfeer@users.noreply.github.com>
2021-10-16 10:23:01 +0300

    Changed the penetration mechanic on the Cernos

--- Items/Weapons/Cernos.cs
+++ Items/Weapons/Cernos.cs
@@ -53,7 +53,7 @@ namespace wdfeerMod.Items.Weapons
         public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
         {
             var proj = ShootWith(position, speedX, speedY, type, damage, knockBack, offset: item.width - 4);
-            if (proj.penetrate < 2 && proj.penetrate != -1) proj.penetrate = 2;
+            if (proj.penetrate != -1) proj.penetrate++;
             proj.usesLocalNPCImmunity = true;
             proj.localNPCHitCooldown = -1;
             return false;
